5f8b9d2d4e202c6b83cd24678b9d0c859145dadd
NhaDevelop <paypanha45@gmail.com>
2024-10-17 14:12:02 +0700

    update

---
+++ DartTree/tempCodeRunnerFile.dart
@@ -0,0 +1,8 @@
+ // Window window1 = Window('Red', 'Left', 1);
+  // Window window2 = Window('Green', 'Right', 1);
+  // Window window3 = Window('Red', 'Left', 2);
+  // Window window4 = Window('Green', 'Right', 2);
+
+  // Roof roof = Roof('Triangle');
+  // Door door = Door('Black');
+  // Chimney chimney = Chimney(true);
